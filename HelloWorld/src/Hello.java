73c09c5c2bd05062b94ceed732140814414309bc
manojvasanthram <manojvasanthram@gmail.com>
2018-05-08 18:10:13 -0700

    Updating Hello.java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
         int myFirstNumber = 5;
         int mySecondNumber = 12;
         int myThirdNumber = 6;
-        
+
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
         System.out.println(myTotal);
