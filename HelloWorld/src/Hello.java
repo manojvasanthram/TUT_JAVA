4f5a4fd2832e0e7d8794c1d69da48c994fc21175
manojvasanthram <manojvasanthram@gmail.com>
2018-05-08 18:00:08 -0700

    First commit of HelloWorld

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,  World! by MVR.");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        System.out.println("Total  = ");
+    }
+}
